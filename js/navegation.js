12ebc710f4294adc7c2388e788fd5036fa79de7c
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2021-08-05 20:24:55 -0500

    Add files via upload

--- js/navegation.js
+++ js/navegation.js
@@ -6,7 +6,11 @@
 
 
 $(function() {
+ 
+     /*     variables       */
 
+     //obtener la informacion del json
+     const json = getJson();
 
 
 
@@ -21,9 +25,57 @@ $(function() {
 });
 
 
-function get_information_about_attention(json,attention,nameEntity,nameAttention,about) {
+function get_information_about_attention(json,objectInsert,attention,nameEntity,about) {
 
+    //variables internas   
+    let dimension;
+    let nameAttention;
+
+    //obtener el nombre de la dimension presionada
+    $('.dimension').each( (index,value)=> {
+        
+         if ( $(value).hasClass('active') ) {
+         dimension = $(value).attr('title');
+         console.log(dimension)
+         }
+
+    })
+
+    //Insertar el texto 
+    json.then( (value) => {
+
+      // no olvidar colocar una restricion para la dimensio individual
+      nameAttention = (Object.keys(value[dimension][attention][nameEntity]))[0]
+      $(objectInsert).html(value[dimension][attention][nameEntity][nameAttention][about])
+      
+    }).catch(  (err) => {
+
+      console.log('ups, paso un error al insertar el texto' , err)
+      
+    });
    
+}
+
+
+async function getJson() {
+
+   const answer = await fetch('dataBase/data.json');
+
+   if (!answer.ok) {
+
+    let error = "Error 404. datAbase Json, no found";
+    console.log("hey")
+    throw new Error(error);
+
+   }
+   else {
+
+   const objectJson = answer.json();
+   return objectJson;
+
+   }
+
+
 }
 
 
