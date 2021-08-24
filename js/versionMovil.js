a2fde6a50aeca1efb339623e36fd18b9597f70bb
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2021-08-24 15:38:15 -0500

    Add files via upload

--- js/versionMovil.js
+++ js/versionMovil.js
@@ -43,7 +43,6 @@ async function breadcrumbs_funcionality(json) {
     let arrows;
     let aux = ['a','div','span']
     let breadcrumbs_array = []
-    let backgroundPosition;
     
     $('.box-breadcrumbs').each((index,value)=> {
     breadcrumbs_array.push(value)
@@ -52,9 +51,8 @@ async function breadcrumbs_funcionality(json) {
     $('.box-breadcrumbs').on('click', function(event) {
        event.preventDefault();
        event.isPropagationStopped();
-     
+    
        arrows = this;
-       backgroundPosition = Number($(arrows).attr('id').replace('bread-','')) -1;
 
       if ($('body').hasClass('active')) {
        //agregar la clase activa a la flecha selecionada
@@ -68,12 +66,10 @@ async function breadcrumbs_funcionality(json) {
            
             //agregar el fondo de color para la primer flecha  
             ($(value).find('a').attr('href') === '#home')?  $(value).find('a').css({
-            'background-color': 'gainsboro'
+            'background-color': '#2271b3'
             }):false 
             $(arrows).addClass('box-breadcrumbs-active')
             
-            $//('body,html').css('background-position-x', ''+10*backgroundPosition+'%')
-
             //quitar las felchas que estan despues de la selecionada
             for (let i = 0 ; i < 3 ; i++) {
             $(breadcrumbs_array[(index+1)+i]).hide()
